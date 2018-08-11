b912ecaa1be4f48809a3955b8269e0157dcfe4b7
GilAriel <32790378+GilAriel@users.noreply.github.com>
2018-08-11 13:56:48 +0300

    Update webserver.sh

--- webserver.sh
+++ webserver.sh
@@ -2,6 +2,6 @@
 
 echo 'Starting Provision: web'$1
 sudo apt-get update
-sudo apt-get install -y nginx -silent
+sudo apt-get install -y nginx -s
 echo "Hello from web server web"$1 >> /usr/share/nginx/html/index.html
 echo 'Provision web'$1 'complete'
